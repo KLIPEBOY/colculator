47ac8622c76d18c494757c81169d8e3f7eba6977
admin <admin@DESKTOP-CNLFK9P>
2023-12-11 13:04:01 +0300

    Добавьте файлы проекта.

---
+++ colculator/colculator.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    char operation;
+    double num1, num2, result;
+    int numExamples = INT_MAX;
+
+
+    for (int i = 0; i < numExamples; i++) {
+        std::cout << "Введите операцию (+, -, *, /): ";
+        std::cin >> operation;
+
+        std::cout << "Введите первое число: ";
+        std::cin >> num1;
+
+        std::cout << "Введите второе число: ";
+        std::cin >> num2;
+
+        switch (operation) {
+        case '+':
+            result = num1 + num2;
+            std::cout << "Результат: " << result << std::endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            std::cout << "Результат: " << result << std::endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "Результат: " << result << std::endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                std::cout << "Результат: " << result << std::endl;
+            }
+            else {
+                std::cout << "Ошибка: деление на ноль!" << std::endl;
+            }
+            break;
+        default:
+            std::cout << "Ошибка: неверная операция!" << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
